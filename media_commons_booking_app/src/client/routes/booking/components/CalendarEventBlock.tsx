8f90bde44ce46e27ab42b56d18b7a6f7ad46dcfb
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-06-30 19:46:48 -0400

    one big calendar block drag

--- media_commons_booking_app/src/client/routes/booking/components/CalendarEventBlock.tsx
+++ media_commons_booking_app/src/client/routes/booking/components/CalendarEventBlock.tsx
@@ -1,30 +1,49 @@
+import { Box, IconButton } from '@mui/material';
+
 import { BookingStatusLabel } from '../../../../types';
-import { Box } from '@mui/material';
+import { Close } from '@mui/icons-material';
 import { EventContentArg } from '@fullcalendar/core';
 import React from 'react';
 import { styled } from '@mui/system';
 
+export const NEW_TITLE_TAG = 'Your Reservation';
+
 interface Props {
   bgcolor: string;
+  isLast: boolean;
+  isNew: boolean;
 }
 
-const Block = styled(Box)<Props>(({ theme, bgcolor }) => ({
+const Block = styled(Box)<Props>(({ theme, bgcolor, isLast, isNew }) => ({
   backgroundColor: bgcolor ?? theme.palette.primary.main,
   border: `2px solid ${bgcolor ?? theme.palette.primary.main}`,
-  borderRadius: '2px',
+  borderRadius: '4px',
   outline: 'none',
   height: '100%',
-  width: '100%',
+  width: isNew && !isLast ? '105%' : '100%',
   overflowX: 'hidden',
   cursor: bgcolor ? 'unset' : 'pointer',
+  padding: '2px 4px',
+  position: 'relative',
 }));
+
+const CloseButton = styled(IconButton)`
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 2px;
+`;
 
 export default function CalendarEventBlock(eventInfo: EventContentArg) {
   const match = eventInfo.event.title.match(/\[(.*?)\]/);
   const title = match ? match[1] : eventInfo.event.title;
 
+  const isNew = eventInfo.event.title === NEW_TITLE_TAG;
+  const index = Number(eventInfo.event.url); // hack
+  const isLast = index === -1;
+
   const backgroundColor = () => {
-    if (!match) {
+    if (isNew) {
       return null;
     }
     let backgroundColor = 'rgba(72, 196, 77, 1)';
@@ -40,8 +59,13 @@ export default function CalendarEventBlock(eventInfo: EventContentArg) {
   };
 
   return (
-    <Block bgcolor={backgroundColor()}>
-      <b>{title}</b>
+    <Block bgcolor={backgroundColor()} isNew={isNew} isLast={isLast}>
+      <b>{isNew && index !== 0 ? '' : title}</b>
+      {isNew && isLast && (
+        <CloseButton>
+          <Close />
+        </CloseButton>
+      )}
     </Block>
   );
 }
