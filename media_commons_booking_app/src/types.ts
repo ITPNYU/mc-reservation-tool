f21a8e64946d465a24fb9751d6eb4620adb46425
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-07-07 14:53:24 -0400

    copy changes from main

--- media_commons_booking_app/src/types.ts
+++ media_commons_booking_app/src/types.ts
@@ -67,14 +67,15 @@ export type CalendarEvent = {
 
 export enum Department {
   ALT = 'ALT',
+  CDI = 'CDI',
   GAMES = 'Game Center',
   IDM = 'IDM',
   ITP = 'ITP / IMA / Low Res',
   MARL = 'MARL',
+  MPAP = 'MPAP',
   MUSIC_TECH = 'Music Tech',
-  RECORDED_MUSIC = 'Recorded Music',
+  OTHER = 'Other',
 }
-
 export type DevBranch = 'development' | 'staging' | 'production' | '';
 
 export type Inputs = {
