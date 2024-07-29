12ecf23d9c5ec5dded7552a9979a2e132475f4a7
riho.takagi <rihohearts@gmail.com>
2024-07-29 18:08:08 -0400

    debug

--- booking-app/components/src/client/routes/booking/BookingForm.tsx
+++ booking-app/components/src/client/routes/booking/BookingForm.tsx
@@ -8,7 +8,30 @@ type BookingFormProps = {
 
 export default function BookingForm({ children }: BookingFormProps) {
   useEffect(() => {
-    console.log("DEPLOY MODE ENVIRONMENT:", process.env.CALENDAR_ENV);
+    console.log(
+      "DEPLOY MODE ENVIRONMENT:",
+      process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+    );
+    console.log(
+      "DEPLOY MODE ENVIRONMENT:",
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+    );
+    console.log(
+      "DEPLOY MODE ENVIRONMENT:",
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+    );
+    console.log(
+      "DEPLOY MODE ENVIRONMENT:",
+      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+    );
+    console.log(
+      "DEPLOY MODE ENVIRONMENT:",
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+    );
+    console.log(
+      "DEPLOY MODE ENVIRONMENT:",
+      process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    );
   }, []);
 
   return (
